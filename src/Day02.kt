c8dc854750c0aca9f12f8de3f274bdbe422f78ba
silvad <duartenrsilva@gmail.com>
2021-12-02 19:03:54 +0000

    Day02 - Part1 and Part2 solved

--- src/Day02.kt
+++ src/Day02.kt
@@ -19,13 +19,31 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
+        var horizontal = 0
+        var depth = 0
+        var aim = 0
 
-        return input.size
+        for (instruction in input) {
+            val direction = instruction.split(" ")[0]
+            val value = instruction.split(" ")[1].toInt()
+
+            when (direction) {
+                "forward" -> {
+                    horizontal += value
+                    depth += aim*value
+                }
+                "down" -> aim += value
+                "up" -> aim -= value
+            }
+        }
+
+        return horizontal*depth
     }
 
+    // tests using the input file limited to the first 9 entries
     val testInput = readInput("Day02-test")
     check(part1(testInput) == 494)
-    check(part2(testInput) == 9)
+    check(part2(testInput) == 1248)
 
     val input = readInput("Day02")
     println(part1(input))
