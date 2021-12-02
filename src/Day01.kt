8a6525f5771fa9c8fa4f95b8b88a96000b241e4d
silvad <duartenrsilva@gmail.com>
2021-12-02 15:10:50 +0000

    Day01 - Part1 and Part2 solved Refactor of Part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,25 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        var temp = 100000
         var depthIncreases = 0
 
-        for (item in input) {
-            if (item.toInt() > temp) {
+        for (i in 0..input.size - 2) {
+            if (input[i].toInt() < input[i+1].toInt()) {
                 depthIncreases += 1
             }
-            temp = item.toInt()
-
         }
         return depthIncreases
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var depthSumIncreases = 0
+        var a: Int
+        var b: Int
+
+        for (i in 0..input.size - 4) {
+            a = input[i].toInt() + input[i+1].toInt() + input[i+2].toInt()
+            b = input[i+1].toInt() + input[i+2].toInt() + input[i+3].toInt()
+
+            if (b > a) {
+                depthSumIncreases += 1
+            }
+        }
+
+        return depthSumIncreases
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01-test")
     check(part1(testInput) == 3)
+    check(part2(testInput) == 2)
 
     val input = readInput("Day01")
     println(part1(input))
